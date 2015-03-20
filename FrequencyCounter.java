9897313f6cd1c87e3dc06e1a0d23c9095249daad
Taylor Huston <taylor.a.huston@gmail.com>
2015-03-19 22:55:08 -0700

    Finish FrequencyCounter test client

--- FrequencyCounter.java
+++ FrequencyCounter.java
@@ -6,6 +6,25 @@ public class FrequencyCounter {
         int minLength = Integer.parseInt(args[0]); //Cut off for the length of word
         SymbolTable<String, Integer> st = new SymbolTable<String, Integer>();
 
-        while (!StdIn.isE)
+        while (!StdIn.isEmpty()) { //Build Symbol Table
+            String word = StdIn.readString();
+            if (word.length() >= minLength) { //If the word meets the minimum length
+                if(!st.contains(word)) { //If the word hasn't already been added
+                    st.put(word, 1);
+                } else {
+                    st.put(word, st.get(word)+1); //Increment the count for that word
+                }
+            }
+        }
+
+        String max = "";
+        st.put(max,0);
+        for(String word : st.keys()) {
+            if (st.get(word) ? st.get(max)) {
+                max = word;
+            }
+        }
+        StdOut.println(max + " " + st.get(max));
+        }
     }
 }
