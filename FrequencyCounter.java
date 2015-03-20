f40161438762e0f19991261b533be95b02715f03
Taylor Huston <taylor.a.huston@gmail.com>
2015-03-19 22:43:40 -0700

    Creat FrequencyCounter class

---
+++ FrequencyCounter.java
@@ -0,0 +1,11 @@
+/**
+ * Created by Taylor on 3/19/2015.
+ */
+public class FrequencyCounter {
+    public static void main(String args[]) {
+        int minLength = Integer.parseInt(args[0]); //Cut off for the length of word
+        SymbolTable<String, Integer> st = new SymbolTable<String, Integer>();
+
+        while (!StdIn.isE)
+    }
+}
