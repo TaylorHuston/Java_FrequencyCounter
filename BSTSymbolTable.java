b060e0bf31ccdeedfb4718269bdfb552305af1b1
Taylor Huston <taylor.a.huston@gmail.com>
2015-06-04 19:46:51 -0700

    Implement min() for BSTSymbolTable

--- BSTSymbolTable.java
+++ BSTSymbolTable.java
@@ -1,5 +1,6 @@
 import java.util.ArrayList;
 import java.util.Iterator;
+import java.util.NoSuchElementException;
 
 /**
  Symbol Table implemented with a Binary Search Tree
@@ -78,6 +79,19 @@ public class BSTSymbolTable<Key extends Comparable<Key>, Value> {
 
     }
 
+    public Value min() {
+        return min(root);
+
+    }
+
+    public Value min(Node current) {
+        if (current.left == null) {
+            return current.val;
+        } else {
+            return min(current.left);
+        }
+    }
+
     public boolean contains(Key searchKey) {
         return get(searchKey) != null;
     }
@@ -104,5 +118,69 @@ public class BSTSymbolTable<Key extends Comparable<Key>, Value> {
 
     public int words() {
         return words;
+    }
+
+    //Test client for BST Symbol Table
+    public static void main(String[] args) {
+        BSTSymbolTable<Integer, String> testBSTST = new BSTSymbolTable<Integer, String>();
+
+//        try {
+//            testBSST.deleteMin();
+//            testBSST.deleteMax();
+//        } catch (NoSuchElementException e) {
+//            StdOut.println(e.getMessage());
+//        }
+
+
+        testBSTST.put(3, "Three");
+        testBSTST.put(1, "One");
+        testBSTST.put(2, "Two");
+        testBSTST.put(4, "Four");
+        testBSTST.put(1, "OneOne");
+
+        for (Integer myInt : testBSTST.keys()) {
+            StdOut.println(myInt + " " + testBSTST.get(myInt));
+        }
+        StdOut.println("Size: " + testBSTST.size());
+        StdOut.println();
+
+        //testBSTST.delete(3);
+
+        for (Integer myInt : testBSTST.keys()) {
+            StdOut.println(myInt + " " + testBSTST.get(myInt));
+        }
+        StdOut.println("Size: " + testBSTST.size());
+        StdOut.println();
+
+//        testBSTST.deleteMin();
+//        testBSTST.deleteMax();
+
+        for (Integer myInt : testBSTST.keys()) {
+            StdOut.println(myInt + " " + testBSTST.get(myInt));
+        }
+        StdOut.println("Size: " + testBSTST.size());
+        StdOut.println();
+
+        testBSTST.put(3, "Three");
+        testBSTST.put(1, "One");
+        testBSTST.put(5, "Five");
+
+        for (Integer myInt : testBSTST.keys()) {
+            StdOut.println(myInt + " " + testBSTST.get(myInt));
+        }
+        StdOut.println("Size: " + testBSTST.size());
+        StdOut.println("Min: " + testBSTST.min());
+//        StdOut.println("Max: " + testBSTST.max());
+//        StdOut.println("Floor of 4: " + testBSTST.floor(4));
+//        StdOut.println("Ceiling of 4: " + testBSTST.ceiling(4));
+//        StdOut.println("Floor of 3: " + testBSTST.floor(3));
+//        StdOut.println("Ceiling of 3: " + testBSTST.ceiling(3));
+//        StdOut.println("Floor of 0: " + testBSTST.floor(0));
+//        StdOut.println("Ceiling of 6: " + testBSTST.ceiling(6));
+//        StdOut.println("Array contains 5: " + testBSTST.contains(5));
+//        StdOut.println("Array contains 6: " + testBSTST.contains(6));
+//        StdOut.println("Key at rank 3: " + testBSTST.select(3));
+
+
     }
 }
