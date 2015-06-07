e5256798a15da52cb46f3244b94f84b7f1a9f505
Taylor Huston <taylor.a.huston@gmail.com>
2015-06-06 21:26:38 -0700

    Copy in basic test cases

--- RBTSymbolTable.java
+++ RBTSymbolTable.java
@@ -1,5 +1,79 @@
+import java.util.NoSuchElementException;
+
 /**
  Symbol Table implemented with a Red Black Tree
  */
-public class RBTSymbolTable {
+public class RBTSymbolTable<Key extends Comparable<Key>, Value> {
+
+    //Test client for BST Symbol Table
+    public static void main(String[] args) {
+//        RBTSymbolTable<Integer, String> testBSTST = new BSTSymbolTable<Integer, String>();
+//
+//        try {
+//            testBSTST.deleteMin();
+//            testBSTST.deleteMax();
+//        } catch (NoSuchElementException e) {
+//            StdOut.println(e.getMessage());
+//        }
+//
+//
+//        testBSTST.put(3, "Three");
+//        testBSTST.put(1, "One");
+//        testBSTST.put(2, "Two");
+//        testBSTST.put(4, "Four");
+//        testBSTST.put(1, "OneOne");
+//        testBSTST.put(10, "Ten");
+//
+//        for (Integer myInt : testBSTST.keys()) {
+//            StdOut.println(myInt + " " + testBSTST.get(myInt));
+//        }
+//        StdOut.println("Size: " + testBSTST.size());
+//        StdOut.println();
+//
+//        testBSTST.delete(3);
+//
+//        for (Integer myInt : testBSTST.keys()) {
+//            StdOut.println(myInt + " " + testBSTST.get(myInt));
+//        }
+//        StdOut.println("Size: " + testBSTST.size());
+//        StdOut.println();
+//
+//        testBSTST.deleteMin();
+//        testBSTST.deleteMax();
+//
+//        for (Integer myInt : testBSTST.keys()) {
+//            StdOut.println(myInt + " " + testBSTST.get(myInt));
+//        }
+//        StdOut.println("Size: " + testBSTST.size());
+//        StdOut.println();
+//
+//        testBSTST.put(3, "Three");
+//        testBSTST.put(1, "One");
+//        testBSTST.put(5, "Five");
+//        testBSTST.put(10, "Ten");
+//
+//        for (Integer myInt : testBSTST.keys()) {
+//            StdOut.println(myInt + " " + testBSTST.get(myInt));
+//        }
+//        StdOut.println("Size: " + testBSTST.size());
+//        StdOut.println();
+//        StdOut.println("Min: " + testBSTST.min().key);
+//        StdOut.println("Max: " + testBSTST.max().key);
+//        StdOut.println("Floor of 4: " + testBSTST.floor(4));
+//        StdOut.println("Ceiling of 4: " + testBSTST.ceiling(4));
+//        StdOut.println("Floor of 3: " + testBSTST.floor(3));
+//        StdOut.println("Ceiling of 3: " + testBSTST.ceiling(3));
+//        StdOut.println("Floor of 0: " + testBSTST.floor(0));
+//        StdOut.println("Ceiling of 11: " + testBSTST.ceiling(11));
+//        StdOut.println("Floor of 7: " + testBSTST.floor(7));
+//        StdOut.println("Ceiling of 7: " + testBSTST.ceiling(7));
+//        StdOut.println("Binary Tree contains 5: " + testBSTST.contains(5));
+//        StdOut.println("Binary Tree contains 6: " + testBSTST.contains(6));
+//        StdOut.println("Key at rank 3: " + testBSTST.select(3));
+//        StdOut.println("Key at rank 5: " + testBSTST.select(5));
+//        StdOut.println("Rank of key 3: " + testBSTST.rank(3));
+//        StdOut.println("Rank of key 10: " + testBSTST.rank(10));
+//
+
+    }
 }
